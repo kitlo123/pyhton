b8b2d373f7155c2035b204129f9c978b7456707a
kitlo123 <qut88@hotmail.com>
2017-10-03 21:10:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from bs4 import BeautifulSoup
 from flask import Flask, request, abort
+import json
+import sys
+import requests
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -6,29 +10,24 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-from botimize import Botimize
+from linebot.models import *
 
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-line_bot_api = LineBotApi(<YOUR_CHANNEL_ACCESS_TOKEN>)
-handler = WebhookHandler(<YOUR_CHANNEL_SECRET>)
-botimize = Botimize(<YOUR_BOTIMIZE_APIKEY>,"line")
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+handler = WebhookHandler(config['line_bot']['Channel_Secret'])
 
-@app.route("/", methods=['POST'])
-def ():
+@app.route("/callback", methods=['POST'])
+def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
+    # print("body:",body)
     app.logger.info("Request body: " + body)
-
-    event = request.get_json()
-    botimize.log_incoming(event)
 
     # handle webhook body
     try:
@@ -36,16 +35,23 @@ def ():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return 'ok'
 
+def weather():
+    r = requests.get('http://www.hko.gov.hk/contentc.htm')
+    print(r.url)
+    return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+    if event.message.text == "weather":
+        content = weather()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return content
+    
+if __name__ == '__main__':
     app.run()
