a97f9ef3c6f2f91b0d923f66a88527b7a3c99fec
kitlo123 <qut88@hotmail.com>
2017-10-03 23:17:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("fuck" + event.message.text)
-    content = "{}: {}".format(event.message.text)
+    content = print(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=content))             
