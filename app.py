583d3f71347b77a5b307c9788622478eef7757ff
kitlo123 <qut88@hotmail.com>
2017-10-03 19:45:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<p>Hello World!</p>"
-    
+   
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded['result'][0]['content']['from']
+    text = decoded['result'][0]['content']['text']
+    #print(json_line)
+    print("使用者：",user)
+    print("內容：",text)
+    sendText(user,text)
+    return 0
+
 if __name__ == '__main__':
     app.run(debug=True)
